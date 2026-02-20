f77f35a8e9186dc68776ca10060cd1a1863a66d2
engmtcdrm <deedsoftheforsaken@gmail.com>
2026-02-20 21:34:25 +0000

    refactor: added some checks and simplified funcs

--- crypt/utils.go
+++ crypt/utils.go
@@ -9,6 +9,10 @@ import (
 // cleanAbsPath takes a path, cleans it, resolves environment variables, expands tilde, and
 // returns the absolute path. It ensures that the path is in a standardized format for further processing.
 func cleanAbsPath(path string) (string, error) {
+	if path == "" {
+		return "", nil
+	}
+
 	cleanPath := filepath.Clean(strings.TrimSpace(path))
 	resolvedPath := resolveEnvVars(cleanPath)
 	expandedPath, err := expandTilde(resolvedPath)
@@ -26,24 +30,27 @@ func cleanAbsPath(path string) (string, error) {
 
 // clearMsg overwrites the contents of a byte slice with zeros clearing sensitive data from memory.
 func clearMsg(s *[]byte) {
-	if s != nil {
-		for i := range *s {
-			(*s)[i] = 0
-		}
+	if s == nil {
+		return
+	}
+
+	for i := range *s {
+		(*s)[i] = 0
 	}
 }
 
 // expandTilde expands the tilde (~) in the given path to the user's home directory.
 func expandTilde(path string) (string, error) {
-	if strings.HasPrefix(path, "~") {
-		home, err := os.UserHomeDir()
-		if err != nil {
-			return "", err
-		}
-		return filepath.Join(home, path[1:]), nil
+	if !strings.HasPrefix(path, "~") {
+		return path, nil
 	}
 
-	return path, nil
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(home, path[1:]), nil
 }
 
 func resolveEnvVars(path string) string {
@@ -56,12 +63,17 @@ func isDirEmpty(dirPath string) (bool, error) {
 	if err != nil {
 		return false, err
 	}
+
 	return len(entries) == 0, nil
 }
 
 // trimSpaceBytes trims leading and trailing ASCII whitespace from a byte slice in-place.
 // Returns a subslice of the original slice, so the underlying array is not copied.
 func trimSpaceBytes(b *[]byte) []byte {
+	if b == nil {
+		return nil
+	}
+
 	start := 0
 	end := len(*b)
 
@@ -73,5 +85,6 @@ func trimSpaceBytes(b *[]byte) []byte {
 	for end > start && ((*b)[end-1] == ' ' || (*b)[end-1] == '\t' || (*b)[end-1] == '\n' || (*b)[end-1] == '\r') {
 		end--
 	}
+
 	return (*b)[start:end]
 }
