476a504bb1185d083a5db4ed5198b9042fd6c3d4
John <deedsoftheforsaken@gmail.com>
2026-04-11 19:15:47 -0400

    chore: update documentation

--- doc.go
+++ doc.go
@@ -1,8 +1,4 @@
 // Package entomb allows for the encryption and decryption of data
 // using the Fernet symmetric encryption algorithm. It includes
-// additional salting, and hashing  to make it
-// harder for attackers to brute-force the key.
-//
-// Anything encrypted with this package can only be decrypted by
-// the original user and machine that encrypted it.
+// additional salting, and hashing to enhance security.
 package entomb
